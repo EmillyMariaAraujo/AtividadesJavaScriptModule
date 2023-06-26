ce409693c6f8d7de524673c15ee47f7d6ed76d99
Emilly Maria Araújo da Silva <emillymaria93@gmail.com>
2023-06-26 16:57:14 -0300

    Atividade de classe, dia 26/06/23, Emilly Maria.
    Atividade de classe do dia 26/06, focada em váriaveis e constantes. Tive um pouco de dificuldade no tipo Undefined e Null, mas estou focada em praticar mais.

---
+++ atividade.01.js
@@ -0,0 +1,55 @@
+//crie 14 variaveis , 2 de cada tipo
+//Number
+var numInteiro = 89;
+console.log(numInteiro);
+
+var numReal = 94.6;
+console.log(numReal);
+
+//String
+var nomeCachorro = "toby";
+console.log(nomeCachorro);
+
+var sobrenome = "araújo";
+console.log(sobrenome);
+
+//Boolean
+var verdadeiro = true;
+var falso = false;
+
+console.log("Você é aluno do start?");
+console.log(verdadeiro);
+
+console.log("Você é da turma de Java?");
+console.log(falso);
+
+//Array
+var coresCanetas = ["azul", "vermelho", "preto"];
+console.log(coresCanetas);
+
+var coresEsmaltes = ["rosa", "azul", "vermelho", "verde", "amarelo"];
+console.log(coresEsmaltes);
+
+//Objetos
+var dadosPessoais = {
+  nome: "Emilly",
+  idade: "18",
+  email: "emillymaria93@gmail.com",
+  fone: "81985045036"
+};
+console.log(dadosPessoais);
+console.log(dadosPessoais.nome);
+
+var alunos = { nomeAluno: "Luan", matriculaAluno: "123456789" };
+console.log(alunos);
+
+//Undefined
+var valorIndefinido;
+console.log("valorIndefinido: " + valorIndefinido);
+
+var futuroValor;
+console.log("futuroValor: " + futuroValor);
+
+//null
+var valorVazio = null;
+console.log("valorVazio: " + valorVazio);
---
+++ atividade.02.js
@@ -0,0 +1,20 @@
+//Duas variáveis usando var.
+var nome = 'Emilly'; 
+console.log (nome);
+
+var idade = '18';
+console.log (idade);
+
+//Duas variáveis usando let.
+let numeroImpar = 10;
+console.log (numeroImpar);
+
+let numeroNegativo = -987;
+console.log (numeroNegativo);
+
+// Duas constantes usando const.
+const cpf = 13423467898
+console.log (cpf)
+
+const matricula = 987652437;
+console.log (matricula);
